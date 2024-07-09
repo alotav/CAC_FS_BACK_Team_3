11c18af435755ea930a797ce3039ec9cb38de37f
FrancoFn <scaramuzzinofn@gmail.com>
2024-07-09 10:54:48 -0300

    Segundo commit

--- productos/models.py
+++ productos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Producto:
-    nombre = models.Charfield(max_length=100)
+    nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_length=10, decimal_places=2)
     stock = models.PositiveIntegerField()
