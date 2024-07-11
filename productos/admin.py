c20f6b21dff2cf2860dd6b26a7319072bd9a0c06
alotav <alonsotaverna@hotmail.com>
2024-07-10 21:51:49 -0300

    agrego admin y clases

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Producto, ProductoNovedades
 
 # Register your models here.
+admin.site.register(Producto)
+admin.site.register(ProductoNovedades)
--- productos/models.py
+++ productos/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Producto:
+# subimos las img de cada producto a media/productos
+class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    precio = models.DecimalField(max_length=10, decimal_places=2)
+    precio = models.DecimalField(max_length=10, decimal_places=2, max_digits=10)
     stock = models.PositiveIntegerField()
+    imagen = models.ImageField(upload_to='productos/', default='productos/default.jpg')
+
+    def __str__(self):
+        return self.nombre
+    
+class ProductoNovedades(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='novedades/', default='productos/default.jpg')
 
     def __str__(self):
         return self.nombre
